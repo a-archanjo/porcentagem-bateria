aa9d24ddbf42fc0e3c86f89ebafdc73353102ffc
Augusto Archanjo <contatop.augustoachanjo@gmail.com>
2026-03-24 10:35:47 -0300

    ID máquina

--- main.py
+++ main.py
@@ -4,13 +4,10 @@ import os
 import dotenv
 import socket
 import uuid
-
+import platform
 
 dotenv.load_dotenv()
 
-import uuid
-import socket
-import platform
 
 id_unico = f"{uuid.getnode()}-{socket.gethostname()}-{platform.system()}"
 print(id_unico)
@@ -19,12 +16,12 @@ while True:
     if bateria is not None:
         bateria_percent = bateria.percent
         if not bateria.power_plugged:
-            print("O computador está usando bateria 🔋")
-            enviar_mensagem(f"O computador está usando bateria 🔋 - {bateria_percent}%", os.getenv("TELEFONE_NUMBER"))
+            print(f"O computador {id_unico} está usando bateria 🔋")
+            enviar_mensagem(f"O computador {id_unico} está usando bateria 🔋 - {bateria_percent}%", os.getenv("TELEFONE_NUMBER"))
             while not bateria.power_plugged:
                 bateria = psutil.sensors_battery()
-            print("O computador foi conectado à energia ⚡")
-            enviar_mensagem(f"O computador foi conectado à energia ⚡ - {bateria_percent}%", os.getenv("TELEFONE_NUMBER"))
+            print(f"O computador {id_unico} foi conectado à energia ⚡")
+            enviar_mensagem(f"O computador {id_unico} foi conectado à energia ⚡ - {bateria_percent}%", os.getenv("TELEFONE_NUMBER"))
 
     else:
         print("Não foi possível detectar bateria")
